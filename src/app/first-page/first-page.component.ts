279a6b9628ee47be295c9c83cd661a2554ac8ed0
riyadali <riyadali@gmail.com>
2018-08-01 16:03:08 -0400

    Update first-page.component.ts

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
-  styleUrls: ['./first-page.component.css']
+  styleUrls: ['./first-page.component.scss']
 })
 export class FirstPageComponent implements OnInit {
 
