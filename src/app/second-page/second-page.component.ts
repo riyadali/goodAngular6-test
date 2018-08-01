1de6b12849934585464cacea000808ca4b11fc24
riyadali <riyadali@gmail.com>
2018-08-01 16:04:49 -0400

    Update second-page.component.ts

--- src/app/second-page/second-page.component.ts
+++ src/app/second-page/second-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-second-page',
   templateUrl: './second-page.component.html',
-  styleUrls: ['./second-page.component.css']
+  styleUrls: ['./second-page.component.scss']
 })
 export class SecondPageComponent implements OnInit {
 
