6baf90d8a15c8022dfce5d8d71004087364510b1
riyadali <riyadali@gmail.com>
2018-08-02 15:40:28 -0400

    Update carousel-example.component.ts

--- src/app/carousel-example/carousel-example.component.ts
+++ src/app/carousel-example/carousel-example.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-carousel-example',
+  templateUrl: './carousel-example.component.html',
+  styleUrls: ['./carousel-example.component.scss']
+})
+export class CarouselExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
