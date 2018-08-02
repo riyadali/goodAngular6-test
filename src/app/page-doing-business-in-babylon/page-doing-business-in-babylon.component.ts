4c5419f2ee367bf5f71c5546eb6f2388030b15c9
riyadali <riyadali@gmail.com>
2018-08-02 16:32:53 -0400

    Update page-doing-business-in-babylon.component.ts

--- src/app/page-doing-business-in-babylon/page-doing-business-in-babylon.component.ts
+++ src/app/page-doing-business-in-babylon/page-doing-business-in-babylon.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-page-doing-business-in-babylon',
+  templateUrl: './page-doing-business-in-babylon.component.html',
+  styleUrls: ['./page-doing-business-in-babylon.component.scss']
+})
+export class PageDoingBusinessInBabylonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
