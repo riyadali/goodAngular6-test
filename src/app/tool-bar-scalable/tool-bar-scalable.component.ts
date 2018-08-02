6286817c157dca2220e2aa3ccf857580f0413607
riyadali <riyadali@gmail.com>
2018-08-02 15:43:25 -0400

    Create tool-bar-scalable.component.ts

---
+++ src/app/tool-bar-scalable/tool-bar-scalable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-bar-scalable',
+  templateUrl: './tool-bar-scalable.component.html',
+  styleUrls: ['./tool-bar-scalable.component.scss']
+})
+export class ToolBarScalableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
